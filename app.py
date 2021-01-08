5b51919a58d16d00cd040c8c268e563ee8bb5313
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-01-08 12:55:29 -0800

    merged with brian's code for cart

---
+++ app.py
@@ -0,0 +1,19 @@
+import jinja2
+from flask import Flask, request, redirect, render_template, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def page():
+    """Display the web page."""
+    return render_template('/index.html')
+
+@app.route('/base')
+def base():
+    """basehtml"""
+    return render_template('/base.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
